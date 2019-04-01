cbf2f0fcfc2acc6cc96c2cdd181a3771ce02b0a0
q64545 <q64545@sina.com>
2019-04-01 17:02:02 +0800

    Delete __init__.py

--- pino_hash_method/__init__.py
+++
@@ -1,5 +0,0 @@
-from __future__ import absolute_import  # absolute import
-from __future__ import division         # accurate div
-from __future__ import print_function   # new print func
-
-from pino_hash_method.pino_hash_op_py import pino_hash_op
